720a0a5ff6cec8e6206a8860eb444780f2ca1d48
Bertrik Sikken <bertrik@sikken.nl>
2022-05-28 00:20:29 +0200

    Update DataAdvancedTest

--- sigfox-receiver/src/test/java/nl/bertriksikken/sigfox/restapi/DataAdvancedTest.java
+++ sigfox-receiver/src/test/java/nl/bertriksikken/sigfox/restapi/DataAdvancedTest.java
@@ -26,14 +26,19 @@ public final class DataAdvancedTest {
         Assert.assertEquals(16, dataAdvanced.seqNumber);
         Assert.assertEquals("Limit", dataAdvanced.lqi);
         Assert.assertEquals(0, dataAdvanced.linkQuality);
-        Assert.assertEquals(0.0, dataAdvanced.fixedLat, 0.01);
-        Assert.assertEquals(0.0, dataAdvanced.fixedLng, 0.01);
+        Assert.assertEquals(0.0, dataAdvanced.fixedLat, 1E-6);
+        Assert.assertEquals(0.0, dataAdvanced.fixedLng, 1E-6);
         Assert.assertEquals("SIGFOX_Netherlands_Hyrde_Networks_BV", dataAdvanced.operatorName);
         Assert.assertEquals(528, dataAdvanced.countryCode);
         Assert.assertEquals("628fd74fb3cabb6565ae81bb", dataAdvanced.deviceTypeId);
         
         ComputedLocation computedLocation = dataAdvanced.computedLocation;
         Assert.assertNotNull(computedLocation);
+        Assert.assertEquals(52.003292529645705, computedLocation.latitude, 1E-6);
+        Assert.assertEquals(5.569001982230841, computedLocation.longitude, 1E-6);
+        Assert.assertEquals(7800, computedLocation.radius);
+        Assert.assertEquals(2, computedLocation.source);
+        Assert.assertEquals(1, computedLocation.status);
         
         String s = dataAdvanced.toString();
         System.out.println(s);
